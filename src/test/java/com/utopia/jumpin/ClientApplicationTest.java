9346bde57d6d9141f8c0bc608a7ee356bde59c9a
fjoseph1313 <cesc.joseph@gmail.com>
2016-11-26 19:02:48 -0500

    First commit project setup

---
+++ src/test/java/com/utopia/jumpin/ClientApplicationTest.java
@@ -0,0 +1,18 @@
+package com.utopia.jumpin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by fjoseph1313 on 11/26/2016.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+public class ClientApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+}
